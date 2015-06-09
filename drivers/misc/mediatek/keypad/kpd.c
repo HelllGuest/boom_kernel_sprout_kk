3f83b176d1a5593d7d14fbe855797e0590b068b6
Anoop Kumar <anoopkumar1119@gmail.com>
2015-06-09 12:11:10 +0530

    Enabled Screen wake control

--- drivers/misc/mediatek/keypad/kpd.c
+++ drivers/misc/mediatek/keypad/kpd.c
@@ -17,6 +17,15 @@
 #include <linux/kpd.h>
 #include <mach/hal_priv_kpd.h>
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+#include <linux/input/sweep2wake.h>
+#endif
+#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+#include <linux/input/doubletap2wake.h>
+#endif
+#endif
+
 #define KPD_NAME    "mtk-kpd"
 #define MTK_KP_WAKESOURCE//this is for auto set wake up source
 
@@ -824,7 +833,16 @@ static int kpd_pdrv_probe(struct platform_device *pdev)
     __set_bit(KPD_PMIC_RSTKEY_MAP, kpd_input_dev->keybit);
 #endif
 
-    kpd_input_dev->dev.parent = &pdev->dev;
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+	sweep2wake_setdev(kpd_input_dev);
+#endif
+#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+	doubletap2wake_setdev(kpd_input_dev);
+#endif
+#endif
+
+kpd_input_dev->dev.parent = &pdev->dev;
     r = input_register_device(kpd_input_dev);
     if (r) {
         printk(KPD_SAY "register input device failed (%d)\n", r);
